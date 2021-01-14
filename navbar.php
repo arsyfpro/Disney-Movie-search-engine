bfd35f57f89c12461231c60c2f662c54559b39d6
arsyfpro <udonorewakoneko@gmail.com>
2021-01-14 15:28:07 +0700

    add script index.php

--- navbar.php
+++ navbar.php
@@ -4,7 +4,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-  <a class="navbar-brand" href="#">Movie Search</a>
+  <a class="navbar-brand" href="#">Disney's Animation</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+	$dbsource = $_GET['source'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?= $dbsource ?>
+</body>
+</html>
